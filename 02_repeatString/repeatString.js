fb1be25d083adc252023db9da3243c9510817882
Shealyn Sugai <shealyn@student.dev-island.org>
2023-06-22 19:44:45 -1000

    Finished Repeat String

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (str, times) {
+  let resultStr = "";
+  if (times < 0) {
+    return "ERROR";
+  }
 
+  for (i = 0; i < times; i++) {
+    resultStr += str;
+  }
+  return resultStr;
 };
 
 // Do not edit below this line
