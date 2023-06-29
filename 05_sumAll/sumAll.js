cba15c5940f4c042e4b1122254d41109bc3d2088
Shealyn Sugai <shealyn@student.dev-island.org>
2023-06-29 12:48:44 -1000

    Finshed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    typeof num1 !== "number" ||
+    typeof num2 !== "number" ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
+  let start = 0,
+    end = 0;
+  //establish start is less than end
+  if (num1 < num2) {
+    start = num1;
+    end = num2;
+  } else {
+    start = num2;
+    end = num1;
+  }
 
+  let sum = 0;
+
+  for (let i = start; i <= end; i++) {
+    sum = sum + i;
+  }
+  return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
