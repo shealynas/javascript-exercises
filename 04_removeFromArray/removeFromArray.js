0c6366830b3b44dbaf8fbf28e61476ef0eb3e456
Shealyn Sugai <shealyn@student.dev-island.org>
2023-07-02 13:17:31 -1000

    Finished repeatString

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, removed) {
+  let newArray = [];
+  for (let count = 0; count < array.length; count++)
+    if (array[count] !== removed) {
+      newArray.push(array[count]);
+    }
+  return newArray;
 };
 
 // Do not edit below this line
