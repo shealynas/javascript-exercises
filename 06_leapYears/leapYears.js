16cf49bdd9aacbfcd18822238263030081479cf7
Shealyn Sugai <shealyn@student.dev-island.org>
2023-07-05 19:57:15 -1000

    leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+// i: number-year
+// o: return boolean
+// c: lines 6 and 7
+// e:
+const leapYears = function (year) {
+  // divisible by 4
+  // not divisible by 100 unless divisible by 400
+  if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
