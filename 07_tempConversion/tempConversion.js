85b1894e56f03e7b73f40e8e0d65f4422675d687
Shealyn Sugai <shealyn@student.dev-island.org>
2023-07-05 20:15:46 -1000

    Finished tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const convertToCelsius = function() {
+// i: temperature that is a number
+// o: number
+// c: number rounded to one decimal place
+const convertToCelsius = function (temp) {
+  const raw = (temp - 32) / 1.8;
+  return parseFloat(raw.toFixed(1));
 };
-
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  const raw = temp * (9 / 5) + 32;
+  return parseFloat(raw.toFixed(1));
 };
-
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
