21b166f19f0149f34e91a5ee2de0e7212d21661e
Shealyn Sugai <shealyn@student.dev-island.org>
2023-07-05 19:35:40 -1000

    Finished removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,9 @@
-const removeFromArray = function (array, removed) {
+const removeFromArray = function (array, ...args) {
   let newArray = [];
+  console.log(args);
+  console.log(typeof args);
   for (let count = 0; count < array.length; count++)
-    if (array[count] !== removed) {
+    if (!args.includes(array[count])) {
       newArray.push(array[count]);
     }
   return newArray;
