ba8d83551f0114be64ebe0dca7f0a5471ca61898
Shealyn Sugai <shealyn@student.dev-island.org>
2023-07-25 19:04:25 -1000

    palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let newStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let stringLength = newStr.length - 1;
+  for (let i = 0; i < newStr.length / 2; i++) {
+    let character = newStr[i];
+    if (character !== newStr[stringLength]) {
+      return false;
+    } else {
+      stringLength--;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
